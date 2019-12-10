150717525868ff798ed353d71d157846355061a0
Huxley <huxhales@gmail.com>
2019-12-10 11:44:44 +1000

    :construction: Adding modules

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 
 const PORT = process.env.PORT || 3030;
